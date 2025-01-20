7e3f739a60608a8f227c538d9bd2bb4c9bd342af
Pranjal0703 <130304687+Pranjal0703@users.noreply.github.com>
2025-01-21 00:31:16 +0530

    na
    new

---
+++ Delta/Js Calculator/appcalc.js
@@ -0,0 +1,7 @@
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
+
+let current_Input = '';
+let previous_Input = '';
+let operator = '';
+hi
